25ff9bd74a795ec533f2da5bf1c5bbd0f1747f7e
nucleon <info@nucleon.hu>
2024-09-15 18:37:16 +0200

    123

--- components/panasonic_ac/esppac_wlan.cpp
+++ components/panasonic_ac/esppac_wlan.cpp
@@ -550,6 +550,8 @@ void PanasonicACWLAN::handle_packet() {
 }
 
 void PanasonicACWLAN::handle_handshake_packet() {
+  
+  ESP_LOGD(TAG, "Handshake buffer size: " + String(this->rx_buffer_.size()));
   if (this->rx_buffer_[2] == 0x00 && this->rx_buffer_[3] == 0x89)  // Answer for handshake 2
   {
     ESP_LOGD(TAG, "Answering handshake [2/16]");
